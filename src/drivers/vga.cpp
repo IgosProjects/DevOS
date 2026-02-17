b259d92716bd357faa9045e638bd2c4ff08f379f
IgoCodes <igor@DESKTOP-5II1JKB.localdomain>
2026-02-17 14:32:37 +0100

    Add VGA driver

--- src/drivers/vga.cpp
+++ src/drivers/vga.cpp
@@ -9,22 +9,31 @@
 */
 
 #include <stdint.h>
+#include <drivers/vga.hpp>
 
 uint8_t CursorX = 0;
 uint8_t CursorY = 0;
 
 // Place in memory where VGA is stored
-uint16_t VGABuffer = (volatile uint16_t*)0xB8000;
+volatile uint16_t* VGABuffer = (volatile uint16_t*)0xB8000;
+uint8_t CurrentVGAColor = 0x0F; 
 
-// VGA driver class
-class VGA {
-    private:
-
-    public:
-        static void PrintCharacter(char Character);
-};
+uint16_t VGAWidth  = 80;
+uint16_t VGAHeight = 25;
 
 // Prints a single character to the screen
-void VGA::PrintCharacter(char Character) {
+void VGA::PrintCharacter(const char Character) {
+    if (Character == '\n') {
+        CursorX = 0;
+        CursorY++; // Move down a line
+    };
 
+    VGABuffer[CursorY * VGAWidth + CursorX] = (CurrentVGAColor << 8) | Character;
+    CursorX++; // Move one row forward
+}
+
+void VGA::PrintString(const char* String) {
+    while (*String) {
+        PrintCharacter(*String++); // Loop until null terminate
+    }
 }
